8506c65f976c44836ee671bd0ffe1adbdb79951a
Charles <charlesarchy09@gmail.com>
2022-11-16 21:30:46 -0600

    intial commit 1.1

--- pages/addItem.tsx
+++ pages/addItem.tsx
@@ -1,28 +1,95 @@
-import React from 'react'
+import React, { FormEvent, useState } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import {useAddress, useContract} from '@thirdweb-dev/react'
+import { useRouter } from 'next/router'
+
 
 
 
 type Props = {}
 
 function addItem({}: Props) {
+    const rout = useRouter();
     const address = useAddress();
-    const { contract, isLoading, error } = useContract('0xEACA7DEB9F1351440615790afacD683983C855bd', "nft-collection")
-    
+    const { contract, isLoading, error } = useContract(process.env.NEXT_PUBLIC_COLLECTION_CONTACT, "nft-collection")
+    const [preview, setPreview] =useState<string>();
+    const [image, setImage] =useState<string>();
+
+    const mintNFT = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (!contract || !address) return;
+        if (!image) {
+            alert('No image selected');
+            return;
+        }
+        const target = e.target as typeof e.target & {
+            name: { value: string };
+            description: { value: string};
+        };
+        if (!target.name.value) {
+            alert('No Item name was given');
+            return;
+        }
+        if (!target.description.value) {
+            alert('No description was given for Item');
+            return;
+        }
+        const metadata = {
+            name: target.name.value,
+            description: target.description.value,
+            image: image,
+        }
+        try {
+            const tx = await contract.mintTo(address, metadata);
+            const receipt = tx.receipt; //the transition receipt
+            const tokenId = tx.id; //grab the token id
+            const nft = await tx.data()// fetch the transaction details of the minted nft
+            if (!receipt) return;
+
+            console.log(receipt, tokenId, nft);
+            rout.push('/')
+
+        }catch (err){
+            console.error(err);
+        }
+    };
+
   return (
     <div>
         <Header/>
-            <main className='max-w-6xl mx-auto p-10 border'>
-                <h1 className='text-4xl'>Add an Item to the Market place</h1>
-                <h2>Item details</h2>
-                <p>By adding an item to the marketplace, you are essentially
+            <main className='max-w-6xl mx-auto p-10 border rounded-lg shadow-md my-10'>
+                <h1 className='text-4xl font-bold '>Add an Item to the Market place</h1>
+                <h2 className='text-xl font-semibold pt-5'>Item details</h2>
+                <p className='pb-5'>By adding an item to the marketplace, you are essentially
                     Minting an NFT of the item into your wallet which you 
                     can then list for Sell.
                 </p>
-                <div>
-
+                <div className='flex flex-col justify-center items-center md:flex-row md:space-x-5 pt-5'>
+                    <img
+                    className='object-contain border'
+                    width={400}
+                    height={400}
+                     src={preview  || "https://res.cloudinary.com/chuckmaster/image/upload/v1668504528/ebayClone/additem_jcfvu3.png" }
+                      />
+                      <form 
+                      onSubmit={mintNFT}
+                      className='flex flex-col flex-1 p-2 space-y-2 justify-center' action="">
+                        <label className='front-light' htmlFor="">Name of item</label>
+                        <input className='formFill' placeholder='Name of item...' type="text" name="name" id='name'/>
+                          <label htmlFor="">Description</label>
+                          <input className='formFill' placeholder='Enter the description..' type="text" name='description' id='description'/>
+                          <label htmlFor="">Image of the Item</label>
+                          <input type="file" onChange={(e) => {
+                            if (e.target.files?.[0]) {
+                                setPreview(URL.createObjectURL(e.target.files[0]));
+                                setImage(e.target.files[0]);
+                            }
+                          }}  name="" id="" />
+                          <button 
+                          type="submit"
+                          className='mx-auto bg-blue-500 font-bold w-56 px-2 py-2 rounded-2xl shadow-xl border text-white hover:bg-white hover:text-blue-500 md:ml-auto '>Add/Mint Item</button>
+                      </form>
                 </div>
             </main>
         <Footer/>
